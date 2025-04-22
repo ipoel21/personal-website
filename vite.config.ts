9bcb3dd8298b855b1383780aab73c76e0f91ffb4
SaiBorg <saifulrohmat8@gmail.com>
2025-04-23 02:24:53 +0700

    update permision domain vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	preview: {
+		host: '0.0.0.0',
+		port: 3000,
+		allowedHosts: ['swebs.top']
+	  }
 });
